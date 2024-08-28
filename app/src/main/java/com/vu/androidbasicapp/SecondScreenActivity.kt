73a41b5b7b6baea1b98f1e781d4b42174e130e6d
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-08-28 20:46:09 +1000

    pass and collect data using intents

--- app/src/main/java/com/vu/androidbasicapp/SecondScreenActivity.kt
+++ app/src/main/java/com/vu/androidbasicapp/SecondScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.vu.androidbasicapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondScreenActivity: AppCompatActivity() {
@@ -8,6 +9,11 @@ class SecondScreenActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val textvalue = intent.getStringExtra(DATA_KEY_MODULE_ID)
+
         setContentView(R.layout.activity_second_screen)
+
+        val textView: TextView = findViewById(R.id.text)
+        textView.text = textvalue
     }
 }
