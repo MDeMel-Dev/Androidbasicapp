fd4c7a1a203de08348c2a4a87b4466df584a84ed
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-08-29 19:19:14 +1000

    implement linearlayout in activity layout file

--- app/src/main/java/com/vu/androidbasicapp/MainActivity.kt
+++ app/src/main/java/com/vu/androidbasicapp/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_alternative)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_alternative)) { v, insets ->
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
