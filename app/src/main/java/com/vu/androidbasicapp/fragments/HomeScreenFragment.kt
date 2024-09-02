7395a22fa381daa19eade0674707ef3d8adf77a9
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-02 19:53:15 +1000

    Implement navigation action using the navgraph and navcontroller

--- app/src/main/java/com/vu/androidbasicapp/fragments/HomeScreenFragment.kt
+++ app/src/main/java/com/vu/androidbasicapp/fragments/HomeScreenFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.vu.androidbasicapp.R
 
 class HomeScreenFragment: Fragment() {
+
+    private lateinit var button: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +26,10 @@ class HomeScreenFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        button = view.findViewById(R.id.button)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_homeScreenFragment_to_dashboardFragment)
+        }
     }
 }
