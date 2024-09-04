d5c836b11869ddf7045f6941b42ea5b647b4bba9
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-04 21:37:05 +1000

    Create retrofit instance which creates an instance of the api

---
+++ app/src/main/java/com/vu/androidbasicapp/home/network/RestfulApiDevRetrofitClient.kt
@@ -0,0 +1,31 @@
+package com.vu.androidbasicapp.home.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class RestfulApiDevRetrofitClient {
+
+    private val BASE_URL = "https://api.restful-api.dev/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(client)
+        .build()
+
+    val apiService: RestfulApiDevService = retrofit.create(RestfulApiDevService::class.java)
+}
