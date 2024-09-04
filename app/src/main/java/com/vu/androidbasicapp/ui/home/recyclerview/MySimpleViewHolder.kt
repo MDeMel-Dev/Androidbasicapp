a1131317ed16e0986b07d25e7b3031008ab083d4
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-05 08:28:28 +1000

    create viewholder class and layout

---
+++ app/src/main/java/com/vu/androidbasicapp/ui/home/recyclerview/MySimpleViewHolder.kt
@@ -0,0 +1,15 @@
+package com.vu.androidbasicapp.ui.home.recyclerview
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vu.androidbasicapp.R
+
+class MySimpleViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val textView: TextView = view.findViewById(R.id.itemtext)
+
+    fun bind(item: String) {
+        textView.text = item
+    }
+}
