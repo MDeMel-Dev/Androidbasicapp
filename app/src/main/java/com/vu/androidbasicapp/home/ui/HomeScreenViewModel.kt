aec7de776635951dd66e20faf1563cbf28006f8f
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-04 21:37:20 +1000

    Use the created api instance in the viewmodel

--- app/src/main/java/com/vu/androidbasicapp/home/ui/HomeScreenViewModel.kt
+++ app/src/main/java/com/vu/androidbasicapp/home/ui/HomeScreenViewModel.kt
@@ -3,6 +3,7 @@ package com.vu.androidbasicapp.home.ui
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vu.androidbasicapp.home.network.RestfulApiDevRetrofitClient
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -10,13 +11,18 @@ import kotlinx.coroutines.launch
 class HomeScreenViewModel: ViewModel() {
 
     val greetingText = MutableStateFlow("Hello Class")
+    val restfulDevApiService = RestfulApiDevRetrofitClient().apiService
 
     init {
         Log.d("nit3213", "HomeScreenViewModel ViewModel injected ")
 
         viewModelScope.launch {
             delay(4000)
-            updateGreetingTextState("Hi the greeting has changed!")
+            val result = restfulDevApiService.getAllObjects()
+            val stringToDisplay = result.map { item ->
+                item.toString() + "\n\n"
+            }
+            updateGreetingTextState(stringToDisplay.toString())
         }
     }
 
