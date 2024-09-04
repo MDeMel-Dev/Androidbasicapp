78d74a16856f07a6b3250d7ba2a3c885a2fa1b21
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-04 21:09:56 +1000

    Add retrofit dependancies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit dependancies
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
 }
