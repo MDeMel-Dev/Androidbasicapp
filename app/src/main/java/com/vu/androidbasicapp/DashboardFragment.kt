384a97c692dff72b49af7f6e11b731342294c954
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-05 17:19:12 +1000

    Use nav args to obtain Response item data passed through by the ViewHolder

--- app/src/main/java/com/vu/androidbasicapp/DashboardFragment.kt
+++ app/src/main/java/com/vu/androidbasicapp/DashboardFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
 
 class DashboardFragment : Fragment() {
+
+    private val arguments: DashboardFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,11 @@ class DashboardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.screenTitle).text = arguments.detail.toString()
     }
 }
