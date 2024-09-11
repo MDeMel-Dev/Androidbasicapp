65e2fba5d50f2b31d34ffe2b77c2c10d01fa4ad7
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-11 19:27:26 +1000

    Clean up unit test class

--- app/src/test/java/com/vu/androidbasicapp/home/ui/recyclerview/MyRecyclerViewAdapterTest.kt
+++ app/src/test/java/com/vu/androidbasicapp/home/ui/recyclerview/MyRecyclerViewAdapterTest.kt
@@ -4,21 +4,24 @@ import com.vu.androidbasicapp.home.data.ResponseItem
 import io.mockk.every
 import io.mockk.spyk
 import io.mockk.verify
-import io.mockk.verifyCount
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 
 class MyRecyclerViewAdapterTest {
+    val exampleResponseItem = ResponseItem(id = "1", objectName = "sdfhvsdjh", dataSection = mapOf("key1" to "value"))
+    val data = listOf(exampleResponseItem, exampleResponseItem.copy(id = "2"), exampleResponseItem.copy(id = "3"))
+    private lateinit var adapter: MyRecyclerViewAdapter
+
+    @Before
+    fun setup() {
+        adapter = spyk(MyRecyclerViewAdapter(navigationFunction = {}))
+        every { adapter.notifyDataSetChanged() } returns Unit
+    }
 
     @Test
     fun `adapter getitemCount() returns the correct item count`() {
-        val exampleResponseItem = ResponseItem(id = "1", objectName = "sdfhvsdjh", dataSection = mapOf("key1" to "value"))
-        val data = listOf(exampleResponseItem, exampleResponseItem.copy(id = "2"), exampleResponseItem.copy(id = "3"))
-        val adapter = spyk(MyRecyclerViewAdapter(navigationFunction = {}))
-        every { adapter.notifyDataSetChanged() } returns Unit
-
-
         adapter.setData(data)
         val result = adapter.getItemCount()
 
@@ -27,27 +30,23 @@ class MyRecyclerViewAdapterTest {
 
     @Test
     fun `adapter setData() calls notifyDataSetChanged() when it's given new data`() {
-        val exampleResponseItem = ResponseItem(id = "1", objectName = "sdfhvsdjh", dataSection = mapOf("key1" to "value"))
-        val data = listOf(exampleResponseItem, exampleResponseItem.copy(id = "2"), exampleResponseItem.copy(id = "3"))
-        val adapter = spyk(MyRecyclerViewAdapter(navigationFunction = {}))
-        every { adapter.notifyDataSetChanged() } returns Unit
-
+        // When
         adapter.setData(data)
 
+        // Then
         verify { adapter.notifyDataSetChanged() }
     }
 
     @Test
     fun `notifyDataSetChanged() is called everytime new data is set to the adapter`() {
-        val exampleResponseItem = ResponseItem(id = "1", objectName = "sdfhvsdjh", dataSection = mapOf("key1" to "value"))
-        val data = listOf(exampleResponseItem, exampleResponseItem.copy(id = "2"), exampleResponseItem.copy(id = "3"))
+        // Given
         val newDataList = listOf(exampleResponseItem, exampleResponseItem.copy(id = "4"), exampleResponseItem.copy(id = "5"))
-        val adapter = spyk(MyRecyclerViewAdapter(navigationFunction = {}))
-        every { adapter.notifyDataSetChanged() } returns Unit
 
+        // When
         adapter.setData(data)
         adapter.setData(newDataList)
 
+        // Then
         verify(exactly = 2) { adapter.notifyDataSetChanged() }
     }
 }
